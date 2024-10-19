5995534b1bc739387c53acc5694f69119093ab38
Chetan <chetanjain2004@gmail.com>
2024-10-20 01:13:01 +0530

    added a component showing latex code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import './App.css';
 import ResumeBuilder from './Components/ResumeBuilder'
 
+import DisplayResume from './Components/DisplayResume.js';
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to the Resume Builder</h1>
-      </header>
-      <main>
-        <ResumeBuilder />
-      </main>
-    </div>
+
+
+ 
+ 
+    const [resumeData, setResumeData] = useState({
+        personalInfo: {
+            name: '',
+            rollNumber: '',
+            courseBranch: '',
+            contactNumber: '',
+            email: '',
+            githubProfile: '',
+            linkedinProfile: '',
+        },
+        education: [
+            {
+                degree: '',
+                institute: '',
+                cgpa: '',
+                year: '',
+            },
+        ],
+        experience: [
+            {
+                title: '',
+                designation: '',
+                timeline: '',
+                description: '',
+            },
+        ],
+        projects: [
+            {
+                name: '',
+                type: '',
+                timeline: '',
+                githubLink: '',
+                description: '',
+            },
+        ],
+        technicalSkills: [
+            {
+                category: '',
+                skills: '',
+            },
+        ],
+        courses: [
+            {
+                category: '',
+                courseName: '',
+            },
+        ],
+        positions: [
+            {
+                title: '',
+                organization: '',
+                timeline: '',
+                description: '',
+            },
+        ],
+        achievements: [
+            {
+                title: '',
+                description: '',
+                year: '',
+            },
+        ],
+    });
+
+    const [errors, setErrors] = useState({});
+    return (
+      <div className="container mx-auto">
+          {/* Pass the state and the setState function to ResumeBuilder */}
+          <ResumeBuilder resumeData={resumeData} setResumeData={setResumeData} errors={errors} setErrors={setErrors}/>
+          {/* Pass the same state to the DisplayResume component */}
+          <DisplayResume resumeData={resumeData} />
+      </div>
   );
 }
 
