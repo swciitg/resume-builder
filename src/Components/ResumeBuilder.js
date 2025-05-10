f0941243f2c7e8e2045e038d5501dfdc8aae6578
SomiranDutta4 <somirandutta69@gmail.com>
2025-05-10 13:52:00 +0530

    things

--- src/Components/ResumeBuilder.js
+++ src/Components/ResumeBuilder.js
@@ -1,33 +1,33 @@
 import { PlusIcon } from '@heroicons/react/24/outline';
-import React from 'react';
+import React, { useState } from 'react';
 
 const ResumeBuilder = ({ resumeData, setResumeData, errors, setErrors }) => {
     const [pdfFile, setPdf] = useState(null)
 
-    const getFile = async () => {
-        console.log('hey')
-        const response = await axios.get('http://localhost:2000/swc/backend/api/getPdf', {
-            responseType: 'blob'
-        })
-        const pdf = response.data;
-        console.log(pdfFile)
-        setPdf(pdf);
-    }
-    const viewThePDF = async () => {
-        if (!pdfFile) return;
-        const pdfViewUrl = URL.createObjectURL(pdfFile);
-        window.open(pdfViewUrl);
-    }
-    const downloadPdf = async () => {
-        if (!pdfFile) return;
+    // const getFile = async () => {
+    //     console.log('hey')
+    //     const response = await axios.get('http://localhost:2000/swc/backend/api/getPdf', {
+    //         responseType: 'blob'
+    //     })
+    //     const pdf = response.data;
+    //     console.log(pdfFile)
+    //     setPdf(pdf);
+    // }
+    // const viewThePDF = async () => {
+    //     if (!pdfFile) return;
+    //     const pdfViewUrl = URL.createObjectURL(pdfFile);
+    //     window.open(pdfViewUrl);
+    // }
+    // const downloadPdf = async () => {
+    //     if (!pdfFile) return;
 
-        const url = URL.createObjectURL(pdfFile);
-        const a = document.createElement('a');
-        a.href = url;
-        a.download = 'cv.pdf';
-        a.click();
-        URL.revokeObjectURL(url);
-    }
+    //     const url = URL.createObjectURL(pdfFile);
+    //     const a = document.createElement('a');
+    //     a.href = url;
+    //     a.download = 'cv.pdf';
+    //     a.click();
+    //     URL.revokeObjectURL(url);
+    // }
 
     const validate = () => {
         let tempErrors = {};
